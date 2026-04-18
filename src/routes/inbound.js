b3698a8eef361356d9ef7119bd67e636c9f3d2de
noahbarker7777 <nbarker7777@gmail.com>
2026-04-18 16:26:48 -0700

    Add files via upload

---
+++ src/routes/inbound.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { setCall } = require('../store');
+
+function handleInbound(req, res) {
+  const callSid = req.body.CallSid;
+  const callerNumber = req.body.From || req.body.To || 'unknown';
+  const conferenceName = 'conf-' + callSid;
+  const wsUrl = process.env.SERVER_URL.replace('https://', 'wss://') + '/media-stream';
+
+  console.log('[inbound] New call ' + callSid + ' from ' + callerNumber);
+
+  setCall(callSid, {
+    state: 'GREETING',
+    conferenceName: conferenceName,
+    callerNumber: callerNumber,
+    aiLegSid: callSid,
+    agentLegSid: null,
+    streamSid: null,
+    timer: null,
+  });
+
+  const twiml = '<?xml version="1.0" encoding="UTF-8"?>' +
+    '<Response>' +
+    '<Start>' +
+    '<Stream url="' + wsUrl + '">' +
+    '<Parameter name="callSid" value="' + callSid + '" />' +
+    '</Stream>' +
+    '</Start>' +
+    '<Dial>' +
+    '<Conference ' +
+    'startConferenceOnEnter="true" ' +
+    'endConferenceOnExit="false" ' +
+    'waitUrl="https://twimlets.com/holdmusic?Bucket=com.twilio.music.classical" ' +
+    'beep="false">' +
+    conferenceName +
+    '</Conference>' +
+    '</Dial>' +
+    '</Response>';
+
+  res.type('text/xml').send(twiml);
+}
+
+module.exports = { handleInbound };
